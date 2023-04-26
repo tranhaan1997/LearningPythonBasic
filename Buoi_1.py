36f1e152ec577993ef718b9a6831b14a5a9d5ccb
tranhaan <tranhaan1997@gmail.com>
2023-04-26 16:37:34 +0700

    upload bai 1

---
+++ Buoi_1.py
@@ -0,0 +1,47 @@
+# print("**  **  ****** **    **     ******")
+# print("**  **  **     **    **     **  **")
+# print("******  ****** **    **     **  **")
+# print("**  **  **     **    **     **  **")
+# print("**  **  ****** ****  ****   ******")
+
+
+# a, a1 = 4,5
+# print(a,a1)
+
+# b, pi = 4, 3.14
+# print("b = %d là số nguyên và pi = %.2f là số thực" % (b,pi))
+
+
+# c = 5,4,2,3,1
+# print(c)
+
+# print(type(c))
+
+# d = 1, 'hello', 3.14
+# print(type(d))
+
+# result = True
+# print(result)
+# print(type(result))
+
+# not_result = not result
+# print(not_result)
+# print(type(not_result))
+
+
+# hello = """
+# **  **  ******  **    **      ******
+# **  **  **      **    **      **  **
+# ******  ******  **    **      **  **
+# **  **  **      **    **      **  **
+# **  **  ******  ***** *****   ******
+# """
+# print(hello)
+# print(type(hello))
+
+hi = 'hello world'
+# print(hi[0])
+# print(hi[0 : 5]) # Mặc định from : index và to : index + 1 
+# print(hi[:5])
+# print(hi[-5:])
+print(len(hi))
