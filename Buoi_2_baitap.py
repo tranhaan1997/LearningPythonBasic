144438c395d0c82ea91c9987450bb4f9e1023f39
tranhaan <tranhaan1997@gmail.com>
2023-05-09 09:22:46 +0700

    update code

---
+++ Buoi_2_baitap.py
@@ -0,0 +1,30 @@
+# Bài 1
+# a = eval(input("Nhập vào số nguyên thứ nhất: "))
+# b = eval(input("Nhập vào số nguyên thứ hai: "))
+
+# tong = a+b
+# trung_binh = (a+b)/2
+
+# print("Tổng của 2 số nguyên %d và %d là: %d" %(a,b,tong))
+# print("Trung bình cộng của 2 số nguyên %d và %d là: %.1f" %(a,b,trung_binh))
+
+# Bài 2
+# x = eval(input("Nhập vào giá trị x: "))
+# bieu_thuc = 1 + x + x**3/3 + x**5/5
+
+# print("Giá trị của biểu thức khi x = %d là: %.2f" %(x,bieu_thuc))
+
+# Bài 3
+# tien = eval(input("Nhập số tiền muốn đổi: "))
+
+# a = tien / 500000
+# print("Số tờ mệnh giá 500,000: %d" %a)
+# a = tien % 500000 / 200000
+# print("Số tờ mệnh giá 200,000: %d" %a)
+# a = tien % 500000 % 200000 / 100000  
+# print("Số tờ mệnh giá 100,000: %d" %a)
+# a = tien % 500000 % 200000 % 100000 / 50000
+# print("Số tờ mệnh giá 50,000: %d" %a)
+# a = tien % 500000 % 200000 % 100000 % 50000
+# print("Số tiền dư là: {:,d}".format(a))
+  
